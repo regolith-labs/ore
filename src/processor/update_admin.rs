9897808b8fad6532fc8b7f6df41edb370e7cca9a
Hardhat Chad <155858888+HardhatChad@users.noreply.github.com>
2024-03-08 09:29:51 -0600

    Update update_admin.rs

--- src/processor/update_admin.rs
+++ src/processor/update_admin.rs
@@ -15,16 +15,16 @@ use crate::{instruction::UpdateAdminArgs, loaders::*, state::Treasury, utils::Ac
 /// Discussion:
 /// - The admin authority has one lever of power: the ability to adjust the global 
 ///   mining difficulty. If the difficulty is too easy, miners will find hashes very quickly
-///   and the bottleneck for throughput shifts from local compute to Solana bandwidth. In essence,
+///   and the bottleneck for mining will shift from local compute to Solana bandwidth. In essence,
 ///   if the Ore token has value and difficulty is low, mining becomes an incentivized stress
 ///   test for the Solana network. 
-/// - At the same time, if difficulty is too hard, miners will have to wait very long periods
+/// - At the same time, if difficulty is too hard, miners will have to wait a very long period
 ///   of time between finding valid hashes. This will bias rewards to well-resourced miners 
 ///   with large compute operations. Keeping a low difficulty ensures casual miners can 
 ///   consistently earn rewards and undercuts some of the advantage of larger players.
 /// - Ultimately admin authority should be delegated to a governance mechanism – either 
 ///   democratic or futarchic – to ensure difficulty is kept at a value that represents the 
-///   interests of ecosystem paricipants. 
+///   values and interests of the whole ecosystem.
 pub fn process_update_admin<'a, 'info>(
     _program_id: &Pubkey,
     accounts: &'a [AccountInfo<'info>],
