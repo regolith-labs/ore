90b1f9280013cee86e802c0bed8123bad73311ef
Grant Findlay <dev@grantfindlay.com>
2024-08-16 07:19:12 +0100

    update smoothing

--- api/src/consts.rs
+++ api/src/consts.rs
@@ -60,7 +60,7 @@ pub const BUS_COUNT: usize = 8;
 
 /// The smoothing factor for reward rate changes. The reward rate cannot change by mCOAL or less
 /// than a factor of this constant from one epoch to the next.
-pub const SMOOTHING_FACTOR: u64 = 10;
+pub const SMOOTHING_FACTOR: u64 = 2;
 
 // Assert MAX_EPOCH_REWARDS is evenly divisible by BUS_COUNT.
 static_assertions::const_assert!(
