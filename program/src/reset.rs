041efb2ff07fd59a611e10554d8c5db09db070ec
Hardhat Chad <hardhatchad@gmail.com>
2024-08-25 12:01:53 +0000

    reset update

--- program/src/reset.rs
+++ program/src/reset.rs
@@ -63,9 +63,9 @@ pub fn process_reset(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramResul
         let bus = Bus::try_from_bytes_mut(&mut bus_data)?;
 
         // Track top balance.
-        if bus.top_balance.gt(&top_balance) {
-            top_balance = bus.top_balance;
-        }
+        // if bus.top_balance.gt(&top_balance) {
+        //     top_balance = bus.top_balance;
+        // }
 
         // Track accumulators.
         total_remaining_rewards = total_remaining_rewards.saturating_add(bus.rewards);
