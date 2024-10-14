681df6f96d90a72f01cad2428c85970ff508b02a
alex <aabinaei@gmail.com>
2024-10-14 01:56:47 -0700

    base rewards

--- program/src/mine.rs
+++ program/src/mine.rs
@@ -123,6 +123,7 @@ pub fn process_mine(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
     //
     // Boosts are incentives that can multiply a miner's rewards by staking tokens in the ORE Boosts program.
     // Up to 3 boosts can be applied on any given mine operation.
+    let base_reward = reward;
     let mut applied_boosts = [Pubkey::new_from_array([0; 32]); 3];
     for i in 0..3 {
         if optional_accounts.len().gt(&(i * 2)) {
@@ -166,7 +167,7 @@ pub fn process_mine(accounts: &[AccountInfo], data: &[u8]) -> ProgramResult {
     }
     // Log base reward after boost rewards.
     // Parser looks for base reward first, and then for the variable number of boost rewards.
-    sol_log(&format!("Base: {}", reward));
+    sol_log(&format!("Base: {}", base_reward));
 
     // Apply liveness penalty.
     //
