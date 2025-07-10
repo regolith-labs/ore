bcded199a85644671d11ede49d4a99236bc8f525
Hardhat Chad <hardhatchad@gmail.com>
2025-07-10 13:18:38 -0700

    no safety log

--- program/src/log.rs
+++ program/src/log.rs
@@ -4,10 +4,10 @@ use steel::*;
 /// No-op, use instruction data for logging w/o truncation.
 pub fn process_log(accounts: &[AccountInfo<'_>], _data: &[u8]) -> ProgramResult {
     // Load accounts.
-    let [signer_info] = accounts else {
-        return Err(ProgramError::NotEnoughAccountKeys);
-    };
-    signer_info.as_account::<Block>(&ore_api::ID)?;
+    // let [signer_info] = accounts else {
+    //     return Err(ProgramError::NotEnoughAccountKeys);
+    // };
+    // signer_info.as_account::<Block>(&ore_api::ID)?;
 
     // For data integrity, only a block can log messages.
 
