43e24c388e6a51ba137bb4c9749b5ef4e6ec175d
Hardhat Chad <hardhatchad@gmail.com>
2025-08-12 11:42:03 -0700

    validate owner

--- program/src/transfer_mint_authority.rs
+++ program/src/transfer_mint_authority.rs
@@ -5,6 +5,8 @@ use steel::*;
 
 const NEW_TREASURY_ADDRESS: Pubkey = pubkey!("45db2FSR4mcXdSVVZbKbwojU6uYDpMyhpEi7cC8nHaWG");
 
+const ORE_V3_PROGRAM_ID: Pubkey = pubkey!("oreV3EG1i9BEgiAJ8b177Z2S2rMarzak4NMv1kULvWv");
+
 /// TransferMintAuthority transfers the mint authority to a new address.
 pub fn process_transfer_mint_authority(
     accounts: &[AccountInfo<'_>],
@@ -21,7 +23,9 @@ pub fn process_transfer_mint_authority(
         .has_address(&ore_api::consts::MINT_ADDRESS)?
         .is_writable()?;
     treasury_info.is_treasury()?.is_writable()?;
-    new_treasury_info.has_address(&NEW_TREASURY_ADDRESS)?;
+    new_treasury_info
+        .has_address(&NEW_TREASURY_ADDRESS)?
+        .has_owner(&ORE_V3_PROGRAM_ID)?;
     token_program.is_program(&spl_token::ID)?;
 
     // Transfer the mint authority to the new treasury.
