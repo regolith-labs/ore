372ba1d77ad430984f97681c8df4e0fb322636ab
Hardhat Chad <hardhatchad@gmail.com>
2025-10-03 11:16:33 -0700

    cleanup

--- api/src/state/miner.rs
+++ api/src/state/miner.rs
@@ -1,49 +1,12 @@
 use steel::*;
 
-use crate::state::{miner_pda, OreAccountOLD};
+use crate::state::miner_pda;
 
 use super::OreAccount;
 
 #[repr(C)]
 #[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
 pub struct Miner {
-    /// The authority of this miner account.
-    pub authority: Pubkey,
-
-    /// The miner's prospects in the current round.
-    pub deployed: [u64; 25],
-
-    /// The cumulative amount of SOL deployed on each square prior to this miner's move.
-    pub cumulative: [u64; 25],
-
-    /// SOL witheld in reserve to pay for checkpointing.
-    pub checkpoint_fee: u64,
-
-    /// The last round that this miner checkpointed.
-    pub checkpoint_id: u64,
-
-    /// The amount of SOL this miner has been refunded.
-    // pub refund_sol: u64,
-
-    /// The amount of SOL this miner can claim.
-    pub rewards_sol: u64,
-
-    /// The amount of ORE this miner can claim.
-    pub rewards_ore: u64,
-
-    /// The ID of the round this miner last played in.
-    pub round_id: u64,
-
-    /// The total amount of SOL this miner has mined across all blocks.
-    pub lifetime_rewards_sol: u64,
-
-    /// The total amount of ORE this miner has mined across all blocks.
-    pub lifetime_rewards_ore: u64,
-}
-
-#[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
-pub struct MinerOLD {
     /// The authority of this miner account.
     pub authority: Pubkey,
 
@@ -82,4 +45,3 @@ impl Miner {
 }
 
 account!(OreAccount, Miner);
-account!(OreAccountOLD, MinerOLD);
