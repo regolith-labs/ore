bbfbb8215703e113e4a1573d76ad09eb724263f5
Hardhat Chad <155858888+HardhatChad@users.noreply.github.com>
2025-10-05 16:03:23 -0500

    Hardhat/refund (#135)
    * refund
    
    * comment
    
    * cleanup
    
    * check

--- program/src/checkpoint.rs
+++ program/src/checkpoint.rs
@@ -28,12 +28,12 @@ pub fn process_checkpoint(accounts: &[AccountInfo<'_>], _data: &[u8]) -> Program
         return Ok(());
     }
 
-    let round = round_info.as_account_mut::<Round>(&ore_api::ID)?;
+    let round = round_info.as_account_mut::<Round>(&ore_api::ID)?; // Round has been closed.
     treasury_info.as_account::<Treasury>(&ore_api::ID)?;
     system_program.is_program(&system_program::ID)?;
 
     // If round is current round, or the miner round ID does not match the provided round, return.
-    if round.id == board.round_id || round.id != miner.round_id {
+    if round.id == board.round_id || round.id != miner.round_id || round.slot_hash == [0; 32] {
         return Ok(());
     }
 
@@ -43,13 +43,6 @@ pub fn process_checkpoint(accounts: &[AccountInfo<'_>], _data: &[u8]) -> Program
         miner.checkpoint_id = miner.round_id;
         return Ok(());
     }
-
-    // Get the RNG.
-    // If the round has no RNG, no one wins.
-    let Some(r) = round.rng() else {
-        miner.checkpoint_id = miner.round_id;
-        return Ok(());
-    };
 
     // Calculate bot fee.
     // If the round expires in less than 12h, anyone may checkpoint this account and collect the bot fee.
@@ -62,43 +55,60 @@ pub fn process_checkpoint(accounts: &[AccountInfo<'_>], _data: &[u8]) -> Program
     // Calculate miner rewards.
     let mut rewards_sol = 0;
     let mut rewards_ore = 0;
-    let winning_square = round.winning_square(r) as usize;
-    if miner.deployed[winning_square] > 0 {
-        // Sanity check.
-        assert!(
-            round.deployed[winning_square] >= miner.deployed[winning_square],
-            "Invalid round deployed amount"
-        );
 
-        // Calculate SOL rewards.
-        let original_deployment = miner.deployed[winning_square];
-        let admin_fee = (original_deployment / 100).max(1);
-        rewards_sol = original_deployment - admin_fee;
-        rewards_sol += ((round.total_winnings as u128 * miner.deployed[winning_square] as u128)
-            / round.deployed[winning_square] as u128) as u64;
+    // Get the RNG.
+    if let Some(r) = round.rng() {
+        // Get the winning square.
+        let winning_square = round.winning_square(r) as usize;
 
-        // Calculate ORE rewards.
-        if round.top_miner == SPLIT_ADDRESS {
-            // If round is split, split the reward evenly among all miners.
-            rewards_ore = ((round.top_miner_reward * miner.deployed[winning_square])
-                / round.deployed[winning_square]) as u64;
-        } else {
-            // If round is not split, payout to the top miner.
-            let top_miner_sample = round.top_miner_sample(r, winning_square);
-            if top_miner_sample >= miner.cumulative[winning_square]
-                && top_miner_sample
-                    < miner.cumulative[winning_square] + miner.deployed[winning_square]
-            {
-                rewards_ore = round.top_miner_reward;
-                round.top_miner = miner.authority;
+        // If the miner deployed to the winning square, calculate rewards.
+        if miner.deployed[winning_square] > 0 {
+            // Sanity check.
+            assert!(
+                round.deployed[winning_square] >= miner.deployed[winning_square],
+                "Invalid round deployed amount"
+            );
+
+            // Calculate SOL rewards.
+            let original_deployment = miner.deployed[winning_square];
+            let admin_fee = (original_deployment / 100).max(1);
+            rewards_sol = original_deployment - admin_fee;
+            rewards_sol += ((round.total_winnings as u128 * miner.deployed[winning_square] as u128)
+                / round.deployed[winning_square] as u128) as u64;
+
+            // Calculate ORE rewards.
+            if round.top_miner == SPLIT_ADDRESS {
+                // If round is split, split the reward evenly among all miners.
+                rewards_ore = ((round.top_miner_reward * miner.deployed[winning_square])
+                    / round.deployed[winning_square]) as u64;
+            } else {
+                // If round is not split, payout to the top miner.
+                let top_miner_sample = round.top_miner_sample(r, winning_square);
+                if top_miner_sample >= miner.cumulative[winning_square]
+                    && top_miner_sample
+                        < miner.cumulative[winning_square] + miner.deployed[winning_square]
+                {
+                    rewards_ore = round.top_miner_reward;
+                    round.top_miner = miner.authority;
+                }
+            }
+
+            // Calculate motherlode rewards.
+            if round.motherlode > 0 {
+                rewards_ore += ((round.motherlode as u128 * miner.deployed[winning_square] as u128)
+                    / round.deployed[winning_square] as u128) as u64;
             }
         }
+    } else {
+        // Sanity check.
+        // If there is no rng, total deployed should have been reset to zero.
+        assert!(
+            round.total_deployed == 0,
+            "Round total deployed should be zero."
+        );
 
-        // Calculate motherlode rewards.
-        if round.motherlode > 0 {
-            rewards_ore += ((round.motherlode as u128 * miner.deployed[winning_square] as u128)
-                / round.deployed[winning_square] as u128) as u64;
-        }
+        // Round has no slot hash, refund all SOL.
+        rewards_sol = miner.deployed.iter().sum::<u64>();
     }
 
     // Checkpoint miner.
@@ -116,6 +126,7 @@ pub fn process_checkpoint(accounts: &[AccountInfo<'_>], _data: &[u8]) -> Program
         miner_info.send(bot_fee, &signer_info);
     }
 
+    // Assert miner account has sufficient funds for rent and rewards.
     let account_size = 8 + std::mem::size_of::<Miner>();
     let required_rent = Rent::get()?.minimum_balance(account_size);
     assert!(
