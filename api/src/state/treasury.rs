caf98b3052367028ca439986b4fd814c78792577
Hardhat Chad <hardhatchad@gmail.com>
2025-10-07 15:47:25 -0700

    cleanup

--- api/src/state/treasury.rs
+++ api/src/state/treasury.rs
@@ -1,7 +1,5 @@
 use steel::*;
 
-use crate::state::OreAccountOLD;
-
 use super::OreAccount;
 
 /// Treasury is a singleton account which is the mint authority for the ORE token and the authority of
@@ -25,24 +23,6 @@ pub struct Treasury {
     pub total_unclaimed: u64,
 }
 
-/// Treasury is a singleton account which is the mint authority for the ORE token and the authority of
-/// the program's global token account.
-#[repr(C)]
-#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
-pub struct TreasuryOLD {
-    // The amount of SOL collected for buy-bury operations.
-    pub balance: u64,
-
-    /// The amount of ORE in the motherlode rewards pool.
-    pub motherlode: u64,
-
-    /// The cumulative ORE distributed to stakers, divided by the total stake at the time of distribution.
-    pub rewards_factor: Numeric,
-
-    /// The current total amount of ORE staking deposits.
-    pub total_staked: u64,
-}
-
 impl Treasury {
     pub fn total_yielding_ore(&self) -> u64 {
         self.total_staked + self.total_unclaimed
@@ -57,5 +37,3 @@ impl Treasury {
 }
 
 account!(OreAccount, Treasury);
-
-account!(OreAccountOLD, TreasuryOLD);
